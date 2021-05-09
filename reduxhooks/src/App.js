817eeca53ffa7cc7e028ee5471c0827cc299d93d
Afajal Meman <afajalm@lucid-green.com>
2021-05-10 01:26:07 +0530

    login and counter UI added

--- reduxhooks/src/App.js
+++ reduxhooks/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,15 @@ function App() {
         >
           Hello Afajal
         </a>
-      </header>
+      </header> */}
+
+      <h1>Hello,  user</h1>
+      <button>Logout</button>
+      <h1>Counter:</h1>
+      <span>
+        <button>Increase counter</button>
+        <button>Decrease counter</button>
+      </span>
     </div>
   );
 }
